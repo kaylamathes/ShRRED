0400fd509d48ad09708fbb9c55bc6c66c7024426
Kayla Mathes <kmathes@umich.edu>
2022-08-23 15:04:00 -0400

    adding root trait summary

--- Fine_root_structure.R
+++ Fine_root_structure.R
@@ -38,7 +38,7 @@ FR_7_25 <- read.csv("googledrive_data/ShRRED_root_structure_7_25_22..csv", na.st
 ###Clean Data frames 
 
 FR_6_26_clean <- FR_6_26%>%
-  select(Label, Type, Plot, Dry_root_weight_g_measured, Dry_root_adjusted_g, percent_root, SoilVol.m3., Length.cm., ProjArea.cm2., SurfArea.cm2., AvgDiam.mm., RootVolume.cm3., Tips, Forks, Crossings)%>%
+  select(Label, Type, Plot, Dry_root_weight_g_measured, Dry_root_adjusted_g,  SoilVol.m3., Length.cm., ProjArea.cm2., SurfArea.cm2., AvgDiam.mm., RootVolume.cm3., Tips, Forks, Crossings)%>%
   filter(!is.na(Dry_root_weight_g_measured))%>%
   cbind(Date = "2022-06-26")
  
@@ -51,7 +51,7 @@ FR_6_26_clean <- FR_6_26%>%
  FR_7_25_clean <- FR_7_25%>%
    select(RHIZO.2017a, Dry_root_weight_g_measured, SoilVol.m3., Length.cm., ProjArea.cm2., SurfArea.cm2., AvgDiam.mm., RootVolume.cm3., Tips, Forks, Crossings)%>%
    filter(!is.na(Dry_root_weight_g_measured))%>%
-   cbind(Date = "2022-07-")
+   cbind(Date = "2022-07-25")
  
  
  ###Add a type and plot column for 7-13 and 7-25 data frames 
@@ -98,7 +98,64 @@ FR_7_13_clean <- FR_7_13_clean%>%
                             RHIZO.2017a == "T4_3" |  RHIZO.2017a == "T4_4" ~ "4" ))
                                  
    
-   
+###Add an adjusted root mass (per ash free weight) But 7-13 and 7-25 will use 95.4% root (from the 7-13 procedure)
+
+FR_7_13_clean <-FR_7_13_clean%>%
+  mutate(Dry_root_adjusted_g = Dry_root_weight_g_measured*0.954)%>%
+  select(!percent_root)
+
+
+FR_7_25_clean <-FR_7_25_clean%>%
+  mutate(Dry_root_adjusted_g = Dry_root_weight_g_measured*0.954)
+  
+###Change the label name in 7-13 and 7-25 files 
+FR_7_13_clean <- rename(FR_7_13_clean, Label = RHIZO.2017a)
+FR_7_25_clean <- rename(FR_7_25_clean, Label = RHIZO.2017a)
+
+##Reordering columns in 7-13 and 7-25 to match 
+FR_7_13_clean <- FR_7_13_clean[, c(1,13,14,2,15,3,4,5,6,7,8,9,10,11,12)]
+
+FR_7_25_clean <- FR_7_25_clean[, c(1,13,14,2,15,3,4,5,6,7,8,9,10,11,12)]
+
+
+##Combine all three data frames together 
+FR_total <- rbind(FR_6_26_clean,FR_7_13_clean,FR_7_25_clean)
+
+####Create Specific Root Length (m/g) as the ratio between Fine-root length and dry root mass
+FR_total$Length.cm. <- as.numeric(FR_total$Length.cm.)
+FR_total$SurfArea.cm2. <- as.numeric(FR_total$SurfArea.cm2.)
+FR_total$AvgDiam.mm. <- as.numeric(FR_total$AvgDiam.mm.)
+
+
+FR_total <- FR_total%>%
+  mutate(SRL_mg = (Length.cm./Dry_root_adjusted_g)/100)
+
+###Summarize Root Traits 
+
+FR_total_summary <- FR_total%>%
+  group_by(Type, Date, Plot)%>%
+  summarize(ave_SRL = mean(SRL_mg), ave_root_mass_g = mean(Dry_root_adjusted_g), ave_root_length = mean(Length.cm.), ave_root_volume = mean(RootVolume.cm3.), ave_root_surf_area = mean(SurfArea.cm2.), ave_root_diam = mean(AvgDiam.mm.))
+
+###Plot root traits
+ggplot(FR_total_summary, aes(x = Date, y = ave_root_mass_g, fill = Type)) +
+  geom_boxplot()
+
+ggplot(FR_total_summary, aes(x = Date, y = ave_root_length, fill = Type)) +
+  geom_boxplot()
+
+ggplot(FR_total_summary, aes(x = Date, y = ave_SRL, fill = Type)) +
+  geom_boxplot()
+
+ggplot(FR_total_summary, aes(x = Date, y = ave_root_volume, fill = Type)) +
+  geom_boxplot()
+
+ggplot(FR_total_summary, aes(x = Date, y = ave_root_surf_area, fill = Type)) +
+  geom_boxplot()
+
+ggplot(FR_total_summary, aes(x = Date, y = ave_root_diam, fill = Type)) +
+  geom_boxplot()
+
+
   
 
 
